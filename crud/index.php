211e566dcb46ad29fe2f0b037d294c1fa8fa7907
jaimeeecampoos <140743173+jaimeeecampoos@users.noreply.github.com>
2026-05-19 18:01:11 +0200

    Create index.php

---
+++ crud/index.php
@@ -0,0 +1,11 @@
+<html>
+<head><title>CRUD - Base de datos Cárcel</title></head>
+<body>
+    <h2>Panel de administración - Base de datos Cárcel</h2>
+    <ul>
+        <li><a href="preso/index.php">Gestión de Presos</a></li>
+        <li><a href="actividad/index.php">Gestión de Actividades</a></li>
+        <li><a href="realiza/index.php">Gestión de Actividades realizadas por Presos</a></li>
+    </ul>
+</body>
+</html>
